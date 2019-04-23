e664f1c59e7b3c457b5b79c68cc27cdaf003a637
elvis <elvis.rozario@freightbro.com>
2019-04-23 09:23:43 +0530

    add, edit delete of a single key

--- controllers/migrationController.js
+++ controllers/migrationController.js
@@ -1,3 +1,34 @@
+const MongoClient = require('mongodb').MongoClient;
+
 exports.runMigration = async function (req, res) {
-    res.json({ status: 200, data: 'test' });
+    MongoClient.connect(req.body.uri, { useNewUrlParser: true }, async function (err, db) {
+        try {
+            if (err) throw err;
+            let dbo = db.db(req.body.database);
+            switch (req.body.action) {
+                case 'add':
+                case 'edit':
+                    newvalue = { '$set': {} };
+                    newvalue['$set'][req.body.key] = req.body.value;
+
+                    response = await dbo.collection(req.body.collection).updateMany({}, newvalue);
+                    db.close();
+                    return res.json({ status: 200, data: `successfully ${req.body.action}ed the required field in ${response.modifiedCount} records` });
+
+                case 'delete':
+                    newvalue = { '$unset': {} };
+                    newvalue['$unset'][req.body.key] = req.body.value;
+
+                    response = await dbo.collection(req.body.collection).updateMany({}, newvalue);
+                    db.close();
+                    return res.json({ status: 200, data: `successfully deleted the required field in ${response.modifiedCount} records` });
+                default:
+                    break;
+            }
+
+        } catch (error) {
+            return res.json({ status: 422, message: error.message });
+        }
+    });
+
 };
